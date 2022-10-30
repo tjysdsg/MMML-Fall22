6a009c03d2f9f7245778eff24675c274a828ef11
tjysdsg <tjy1018543509@163.com>
2022-10-30 18:13:27 -0400

    Use evaluation server to submit result

--- vlp/infer_retrieval.sh
+++ vlp/infer_retrieval.sh
@@ -40,3 +40,25 @@ python run_webqa.py \
   --feature_folder $rcnn_feat \
   --detectron_dir $detectron_dir \
   --image_id_map_path $imgid_map
+
+# Or you can specify the exact model path
+# model_ckpt=$webqa_dir/baseline_finetuned/retrieval_x101fpn/model.3.bin
+# python run_webqa.py \
+#   --do_predict \
+#   --split ${split} \
+#   --answer_provided_by 'img' \
+#   --task_to_learn 'filter' \
+#   --use_x_distractors \
+#   --amp \
+#   --new_segment_ids \
+#   --train_batch_size 1 \
+#   --num_workers 4 \
+#   --max_pred 10 --mask_prob 1.0 \
+#   --output_dir ${output_dir} \
+#   --ckpts_dir ${ckpts_dir} \
+#   --txt_dataset_json_path $dataset \
+#   --img_dataset_json_path $dataset \
+#   --feature_folder $rcnn_feat \
+#   --detectron_dir $detectron_dir \
+#   --model_recover_path $model_ckpt \
+#   --image_id_map_path $imgid_map
