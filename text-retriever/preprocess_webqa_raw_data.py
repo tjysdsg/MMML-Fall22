2dcd8836ef46f01ee452d77547c802e3cb244509
lwaekfjlk <1125027232@qq.com>
2022-10-30 23:35:57 -0400

    update preprocess scripts for sub webqa data and full webqa data

--- text-retriever/preprocess_webqa_raw_data.py
+++ text-retriever/preprocess_webqa_raw_data.py
@@ -2,39 +2,44 @@ import json
 import random
 import jsonlines
 
-def generate_dataset_from_raw_WebQA(file_name):
+def generate_dataset_from_raw_WebQA(file_name, split):
     with open(file_name, 'r') as f:
-        dataset = json.load(f)
-        val_dataset = []
-        train_dataset = []
-        for data_id, data in dataset.items():
+        raw_dataset = json.load(f)
+        dataset = []
+        for data_id, data in raw_dataset.items():
             Qcate = data['Qcate']
             Q = data['Q'].replace('"', "")
             A = data['A'][0].replace('"', "")
             txt_facts = []
-            for pos_fact in data['txt_posFacts']:
-                fact = {}
-                fact['title'] = pos_fact['title']
-                fact['fact'] = pos_fact['fact']
-                fact['label'] = 1
-                #txt_facts.append(fact)
-                if data['split'] == 'train':
-                    train_dataset.append({'Q': Q, 'A': A, 'txt_facts': fact})
-                elif data['split'] == 'val':
-                    val_dataset.append({'Q': Q, 'A': A, 'txt_facts': fact})
-            for neg_fact in data['txt_negFacts']:
-                fact = {}
-                fact['title'] = neg_fact['title']
-                fact['fact'] = neg_fact['fact']
-                fact['label'] = 0
-                #txt_facts.append(fact)
-                if data['split'] == 'train':
-                    train_dataset.append({'Q': Q, 'A': A, 'txt_facts': fact})
-                elif data['split'] == 'val':
-                    val_dataset.append({'Q': Q, 'A': A, 'txt_facts': fact})
-    random.shuffle(train_dataset)
-    random.shuffle(val_dataset)
-    return train_dataset, val_dataset
+            for txt_fact_type in ['txt_posFacts', 'txt_negFacts']:
+                for txt_fact in data[txt_fact_type]:
+                    fact = {}
+                    fact['title'] = txt_fact['title']
+                    fact['fact'] = txt_fact['fact']
+                    if txt_fact_type == 'txt_posFacts':
+                        fact['label'] = 1
+                    else:
+                        fact['label'] = 0
+                    if split == 'train' and data['split'] == 'train':
+                        dataset.append({'Q': Q, 'A': A, 'txt_fact': fact})
+                    elif split == 'val' and data['split'] == 'val':
+                        dataset.append({'Q': Q, 'A': A, 'txt_fact': fact})
+
+            for img_fact_type in ['img_posFacts', 'img_negFacts']:
+                for img_fact in data['img_posFacts']:
+                    fact = {}
+                    fact['title'] = img_fact['title']
+                    fact['caption'] = img_fact['caption']
+                    if img_fact_type == 'img_posFacts':
+                        fact['label'] = 1
+                    else:
+                        fact['label'] = 0
+                    if split =='train' and data['split'] == 'train':
+                        dataset.append({'Q': Q, 'A': A, 'img_fact': fact})
+                    elif split == 'val' and data['split'] == 'val':
+                        dataset.append({'Q': Q, 'A': A, 'img_fact': fact})
+    random.shuffle(dataset)
+    return dataset
 
 
 def write_dataset(dataset, output_file_name):
@@ -44,7 +49,21 @@ def write_dataset(dataset, output_file_name):
 
 
 if __name__ == '__main__':
+    '''
+    # for deliberately split small-size WebQA subdata
+    file_name = './raw_data/WebQA_subdata/train_subWebqa.json'
+    train_dataset = generate_dataset_from_raw_WebQA(file_name, split='train')
+    file_name = './raw_data/WebQA_subdata/val_subWebqa.json'
+    val_dataset = generate_dataset_from_raw_WebQA(file_name, split='val')
+    write_dataset(train_dataset, './data/WebQA_sub_data/train.jsonl')
+    write_dataset(val_dataset, './data/WebQA_sub_data/val.jsonl')
+    '''
+
+    # for full-size WebQA data
     file_name = './raw_data/WebQA_data_first_release/WebQA_train_val.json'
-    train_dataset, val_dataset = generate_dataset_from_raw_WebQA(file_name)
-    write_dataset(train_dataset, './data/train.jsonl')
-    write_dataset(val_dataset, './data/val.jsonl')
+    train_dataset = generate_dataset_from_raw_WebQA(file_name, split='train')
+    val_dataset = generate_dataset_from_raw_WebQA(file_name, split='val')
+    write_dataset(train_dataset, './data/WebQA_full_data/train.jsonl')
+    write_dataset(val_dataset, './data/WebQA_full_data/val.jsonl')
+
+
