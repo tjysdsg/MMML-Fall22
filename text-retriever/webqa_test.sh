b8cdb0b36073bd350eaafb595f77a045f1e3f241
lwaekfjlk <1125027232@qq.com>
2022-10-31 16:28:37 -0400

    milestone commit, reached 75.73 F1 on retrieval task using webqa_finetune_full_data_v2.sh's first training checkpoint

--- text-retriever/webqa_test.sh
+++ text-retriever/webqa_test.sh
@@ -1,6 +1,6 @@
 export CUDA_VISIBLE_DEVICES=3
 export NGPU=1
-python -m torch.distributed.launch --nproc_per_node=$NGPU --master_port 29518 main.py \
+python -m torch.distributed.launch --nproc_per_node=$NGPU --master_port 29528 main.py \
 --test \
 --cache_dir ./cache/WebQA_test_data_cache/ \
 --dataset_dir ./data/WebQA_test_data/ \
