23db5b207b3141ffa40f5c438dd0d99f866f28a1
lwaekfjlk <1125027232@qq.com>
2022-11-07 14:47:29 -0500

    add initial version of text generator

---
+++ text-generator/dataset.py
@@ -0,0 +1,238 @@
+import os
+import jsonlines
+import argparse
+import torch
+import random
+from torch.utils.data import Dataset
+from torch.nn.utils.rnn import pad_sequence
+
+class WebQATestDataset(Dataset):
+    def __init__(self, args, tokenizer):
+        self.args = args
+        self.tokenizer = tokenizer
+        self.data = self.build_dataset()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def recursive_tokenize(self, data):
+        if isinstance(data, int):
+            return data
+        elif isinstance(data, str):
+            return self.tokenizer(data.strip(), truncation=True, add_special_tokens=False)['input_ids']
+        elif isinstance(data, dict):
+            for key, value in data.items():
+                if key == 'image_id' or key == 'snippet_id' or key == 'Q_id':
+                    data[key] = value
+                else:
+                    data[key] = self.recursive_tokenize(value)
+
+            return data
+        
+        lists = list(self.recursive_tokenize(subdata) for subdata in data)
+        return lists    
+
+    def build_dataset(self):
+        if self.args.have_cached_dataset:
+            dataset = torch.load(os.path.join(self.args.cache_dir, 'WebQA_test_dataset'))
+        else:
+            with jsonlines.open(os.path.join(self.args.dataset_dir, self.args.test_file), 'r') as jsonl_f:
+                dataset = [obj for obj in jsonl_f]
+            
+            print('=====begin tokenize======')
+            dataset = self.recursive_tokenize(dataset)
+            print('=====end   tokenize======')
+            torch.save(dataset, os.path.join(self.args.cache_dir, 'WebQA_test_dataset'))
+        return dataset
+
+    def collate_fn(self, batch, max_length=None):
+        input_ids = []
+        labels = []
+        attention_mask = []
+        source_types = []
+        q_ids = []
+        source_ids = []
+
+        for instance in batch:
+            instance_token_ids = [self.tokenizer.cls_token_id]
+            # QA part
+            instance_token_ids += instance['Q']
+            #instance_token_ids += instance['A'] # answer is all empty in test cases
+            # add one [SEP] after QA part
+            instance_token_ids += [self.tokenizer.sep_token_id]
+            # fact part (both for text and image)
+            if 'txt_fact' in instance.keys():
+                instance_token_ids += instance['txt_fact']['fact']
+                source_types.append('txt')
+                source_ids.append(instance['txt_fact']['snippet_id'])
+            elif 'img_fact' in instance.keys():
+                instance_token_ids += instance['img_fact']['caption']
+                source_types.append('img')
+                source_ids.append(instance['img_fact']['image_id'])
+            else:
+                raise ValueError('instance should either be image-based or text-based')
+
+            instance_token_ids = instance_token_ids[:self.args.max_length-1] # since there is one last [SEP]
+            # add [SEP] after truncation
+            instance_token_ids += [self.tokenizer.sep_token_id]
+            instance_token_ids = torch.LongTensor(instance_token_ids)
+
+            input_ids.append(instance_token_ids)
+            q_ids.append(instance['Q_id'])
+
+
+        input_ids = pad_sequence(
+            input_ids, 
+            batch_first=True, 
+            padding_value=self.tokenizer.pad_token_id)
+        attention_mask = input_ids.ne(self.tokenizer.pad_token_id)
+
+
+        return {
+            'input_ids': input_ids,
+            'attention_mask': attention_mask,
+            'source_ids': source_ids,
+            'source_types': source_types,
+            'q_ids': q_ids,
+        }
+
+
+
+class WebQADataset(Dataset):
+    def __init__(self, 
+                 args, 
+                 tokenizer, 
+                 split,
+                 question_prefix='question:',
+                 title_prefix='title:',
+                 passage_prefix='context:'):
+        self.args = args
+        self.tokenizer = tokenizer
+        self.data = self.build_dataset(split=split)
+        self.question_prefix = tokenizer.encode(question_prefix, add_special_tokens=False)
+        self.passage_prefix = tokenizer.encode(passage_prefix, add_special_tokens=False)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def recursive_tokenize(self, data):
+        if isinstance(data, int):
+            return data
+        elif isinstance(data, str):
+            return self.tokenizer(data.strip(), truncation=True, add_special_tokens=False)['input_ids']
+        elif isinstance(data, dict):
+            return dict((key, self.recursive_tokenize(value)) for key, value in data.items())
+        
+        lists = list(self.recursive_tokenize(subdata) for subdata in data)
+        return lists    
+
+    def build_dataset(self, split):
+        if self.args.have_cached_dataset:
+            dataset = torch.load(os.path.join(self.args.cache_dir, 'WebQA_{}_dataset'.format(split)))
+        else:
+            if split == 'train':
+                with jsonlines.open(self.args.train_file, 'r') as jsonl_f:
+                    dataset = [obj for obj in jsonl_f]
+            elif split == 'val':
+                with jsonlines.open(self.args.val_file, 'r') as jsonl_f:
+                    dataset = [obj for obj in jsonl_f]
+            else:
+                raise ValueError('no right dataset split')
+            
+        print('=====begin tokenize======')
+        dataset = self.recursive_tokenize(dataset)
+        print('=====end   tokenize======')
+        torch.save(dataset, os.path.join(self.args.cache_dir, 'WebQA_{}_dataset'.format(split)))
+        return dataset
+
+    def collate_fn(self, batch):
+        input_ids = []
+        labels = []
+        attention_mask = []
+        bsz = len(batch)
+
+        for instance in batch:
+            token_facts = []
+            tokens_question = self.question_prefix + instance['Q']
+            for pos_txt_fact in instance['pos_txt_facts']:
+                token_facts += self.passage_prefix + pos_txt_fact['fact']
+            for pos_img_fact in instance['pos_img_facts']:
+                token_facts += self.passage_prefix + pos_img_fact['caption']
+
+            batch_input_ids = tokens_question + token_facts
+            batch_input_ids = batch_input_ids[:self.args.encoder_max_length-1]
+            batch_input_ids += [self.tokenizer.eos_token_id]
+            batch_input_ids = torch.LongTensor(batch_input_ids)
+            
+            input_ids += [batch_input_ids] # get bsz x seq_len 
+
+            tokens_answer = instance['A']
+            batch_labels = tokens_answer  
+            batch_labels = batch_labels[:self.args.decoder_max_length-1]
+            batch_labels = torch.LongTensor(batch_labels)
+            labels += [batch_labels] # get bsz x seq_len
+
+        input_ids = pad_sequence(
+            input_ids, 
+            batch_first=True, 
+            padding_value=self.tokenizer.pad_token_id
+        )
+
+        labels = pad_sequence(
+            labels,
+            batch_first=True,
+            padding_value=-100,
+        )
+        
+        attention_mask = input_ids.ne(self.tokenizer.pad_token_id)
+        decoder_attention_mask = labels.ne(-100)
+
+        return {
+            'input_ids': input_ids.to(self.args.device),
+            'labels': labels.to(self.args.device),
+            'attention_mask': attention_mask.to(self.args.device),
+            'decoder_attention_mask': decoder_attention_mask.to(self.args.device),
+        }
+
+
+if __name__ == '__main__':
+    from transformers import AutoTokenizer
+    from torch.utils.data import DataLoader, SequentialSampler
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cache_dir', type=str, default='./cache', help='the location of cache file')
+    parser.add_argument('--have_cached_dataset', action='store_true')
+    parser.add_argument('--dataset_dir', type=str, default='./data/')
+    parser.add_argument('--model_name', type=str, default='bert-base-uncased', help='model name or path')
+    parser.add_argument('--train_file', type=str, default='train.jsonl', help='path to train file, jsonl for scirex, conll for sciner')
+    parser.add_argument('--val_file', type=str, default='val.jsonl', help='path to dev file')
+    parser.add_argument('--test_file', type=str, default='val.jsonl', help='path to test file')
+    parser.add_argument('--max_length', type=int, default=512)
+    args = parser.parse_args()
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+    train_dataset = WebQADataset(args, tokenizer, 'train')
+    val_dataset = WebQADataset(args, tokenizer, 'val')
+    train_dataloader = Dataloader(train_dataset,
+                                  batch_size=2,
+                                  sampler=SequentialSampler(val_dataset),
+                                  collate_fn=train_dataset.collate_fn)
+    val_dataloader = Dataloader(val_dataset,
+                                batch_size=2,
+                                sampler=SequentialSampler(val_dataset),
+                                collate_fn=val_dataset.collate_fn)
+
+    for data in train_dataloader:
+        input_ids = data['input_ids']
+        labels = data['labels']
+        mask = data['attention_mask']
+        # if input_ids.shape[1] != labels.shape[1] or labels.shape[1] != mask.shape[1]:
+        print(input_ids)
+        print(labels)
+        print(mask)
+        break
---
+++ text-generator/main.py
@@ -0,0 +1,359 @@
+import os
+import torch
+import argparse
+import time
+import csv
+import shutil
+import evaluate
+import logging
+import wandb
+import torch.distributed as dist
+import torch.nn.functional as F
+from tqdm import tqdm, trange
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+from transformers import AdamW, get_cosine_schedule_with_warmup
+from dataset import WebQADataset, WebQATestDataset
+from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pad_sequence
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torchcrf import CRF
+from evaluation import ems
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+def set_seed(args):
+    os.environ['PYTHONHASHSEED'] = str(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
+
+def set_wandb(args):
+    if args.use_wandb:
+        # need to change to your own API when using
+        #os.environ['WANDB_API_KEY'] = '972035264241fb0f6cc3cab51a5d82f47ca713db'
+        wandb.init(project="WebQA", name=args.timestamp, config=args, dir='../WebQA_tmp')
+    return
+
+
+def attach_dataloader(args, tokenizer):
+    loader_dict = {}
+    if args.train:
+        train_dataset = WebQADataset(args, tokenizer, 'train')
+        dev_dataset = WebQADataset(args, tokenizer, 'val')
+        train_dataloader = DataLoader(
+            train_dataset,  
+            batch_size=args.train_batch_size, 
+            shuffle=True, 
+            collate_fn=train_dataset.collate_fn
+        )
+        dev_dataloader = DataLoader(
+            dev_dataset, 
+            batch_size=args.dev_batch_size, 
+            shuffle=True, 
+            collate_fn=dev_dataset.collate_fn
+        )
+        loader_dict['train'] = train_dataloader
+        loader_dict['dev'] = dev_dataloader
+
+    if args.inference:
+        test_dataset = WebQATestDataset(args, tokenizer, 'test')
+        test_dataloader = DataLoader(
+            test_dataset, 
+            batch_size=args.test_batch_size, 
+            shuffle=False, 
+            collate_fn=test_dataset.collate_fn
+        )
+        loader_dict['test'] = test_dataloader
+    
+    return loader_dict
+
+
+def attach_tokenizer(args):
+    return T5Tokenizer.from_pretrained(args.model_name)
+
+
+def attach_model(args):
+    if args.model_type == 't5':
+        model = T5ForConditionalGeneration.from_pretrained(args.model_name)
+    else:
+        raise ValueError('Invalid model type')
+    device = torch.device('cuda')
+    model.to(device)
+
+    if args.load_from_ckpt:
+        model_dict = torch.load(args.load_from_ckpt)
+        model.load_state_dict(model_dict, strict=False)
+    return model
+
+
+def attach_optimizer(args, model):
+    if args.optimizer_type == 'adamw':
+        optimizer = AdamW(model.parameters(), lr=args.learning_rate, correct_bias=False)
+    else:
+        raise ValueError('Invalid optimizer type')
+    return optimizer
+
+
+def attach_scheduler(args, optimizer, train_dataloader):
+    train_steps_per_epoch = len(train_dataloader)
+    total_training_steps = args.num_epochs * train_steps_per_epoch // args.gradient_accumulation_step
+    total_warmup_steps = total_training_steps // 10
+    if args.scheduler_type == 'cosine':
+        scheduler = get_cosine_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=total_warmup_steps,
+            num_training_steps=total_training_steps,
+        )
+        return scheduler
+    else:
+        raise ValueError('Invalid scheduler type')
+
+
+def save_model(best_ckpt_name, metric, best_metric):
+    eps = 1e-5
+    if (args.model_chosen_metric == 'em' and metric['em'] > best_metric['em'] + eps):
+        if best_ckpt_name is not None:
+            os.remove(os.path.join(args.ckpt_save_dir,best_ckpt_name))
+        best_ckpt_name = 'best_{}4{}_{}_{}_{}.ckpt'.format(
+            args.model_type, 
+            args.task, 
+            args.model_chosen_metric, 
+            round(metric[args.model_chosen_metric],3), 
+            args.timestamp
+        )
+        output_model_file = os.path.join(args.ckpt_save_dir, best_ckpt_name)
+        model_to_save = model.module if hasattr(model, 'module') else model
+        torch.save(model_to_save.state_dict(), output_model_file)
+        best_metric[args.model_chosen_metric] = metric[args.model_chosen_metric]
+    return best_ckpt_name, best_metric
+
+
+def save_final_model(best_ckpt_name):
+    src_file = os.path.join(args.ckpt_save_dir, best_ckpt_name)
+    tgt_file = os.path.join(args.ckpt_save_dir, 'best_{}4{}.ckpt'.format(args.model_type, args.task))
+    shutil.copy(src_file, tgt_file)
+    return
+
+
+def validate(args, dev_dataloader, model, tokenizer):
+    model.eval()
+
+    losses = []
+    refs = []
+    preds = []
+    with torch.no_grad():
+        for batch in tqdm(dev_dataloader):
+            pred_tokens = model.generate(
+                input_ids=batch['input_ids'],
+                attention_mask=batch['attention_mask'],
+                max_length=30,
+                num_beams=5,
+            )
+            ref_tokens = batch['labels']
+            ref_tokens[ref_tokens == -100] = tokenizer.pad_token_id
+            pred = tokenizer.batch_decode(pred_tokens, skip_special_tokens=True)
+            ref = tokenizer.batch_decode(ref_tokens, skip_special_tokens=True)
+            preds += pred
+            refs += ref
+    assert len(preds) == len(refs)
+
+    em = 0 
+    for pred, ref in zip(preds, refs):
+        em += ems(pred, ref)
+    em = em / len(preds)
+
+    return {'em': em}
+
+
+def train(args, model, tokenizer):
+    best_ckpt_name = None
+    best_metric = {'em': -float('inf'), 'loss': float('inf')}
+    step = 0
+    iteration = 0
+    logging.info('=====begin loading dataset====')
+    loaders = attach_dataloader(args, tokenizer)
+    logging.info('=====end loading dataset====')
+    train_dataloader = loaders['train']
+    dev_dataloader = loaders['dev']
+    
+    optimizer = attach_optimizer(args, model)
+    scheduler = attach_scheduler(args, optimizer, train_dataloader)
+    model.train()
+
+    step_losses = []
+    scaler = torch.cuda.amp.GradScaler(enabled=args.use_fp16)
+    train_iter = trange(args.num_epochs, desc="Epoch", disable=-1)
+    for epoch in train_iter:
+        epoch_iter = tqdm(train_dataloader, desc="Iteration", disable=0)
+        for batch in epoch_iter:
+            model.train()
+            with torch.cuda.amp.autocast(enabled=args.use_fp16):
+                outputs = model(
+                    input_ids=batch['input_ids'],
+                    labels=batch['labels'],
+                    decoder_attention_mask=batch['decoder_attention_mask'],
+                    attention_mask=batch['attention_mask'],
+                )
+                loss = outputs.loss
+                scaler.scale(loss).backward()
+                step_losses.append(loss.item())
+
+            iteration += 1
+            if iteration % args.gradient_accumulation_step == 0:
+                torch.nn.utils.clip_grad_norm_(
+                    parameters=model.parameters(), 
+                    max_norm=args.max_norm
+                )
+                scaler.step(optimizer)
+                scaler.update()
+                optimizer.zero_grad()
+                scheduler.step()
+                
+                step += 1
+                if step % args.evaluation_steps == 0:
+                    metric = validate(args, dev_dataloader, model, tokenizer)
+                    best_ckpt_name, best_metric = save_model(best_ckpt_name, metric, best_metric)
+                    logging.info('eval em : {}'.format(metric['em']))
+
+                    if args.use_wandb:
+                        wandb.log({'train loss': sum(step_losses)/len(step_losses), 'step': step})
+                        wandb.log({'learning rate': scheduler.get_last_lr()[0], 'step': step})
+                        wandb.log({'eval_em': metric['em'], 'step': step})
+                    step_losses = []
+                    
+    save_final_model(best_ckpt_name)
+    return
+
+
+def ner_pipeline(args, sent, model, tokenizer):
+    tokenized_sent = tokenizer.tokenize(sent)
+    input_ids = tokenizer.encode(sent)
+    token_starts = [0] + [1 - int(token.startswith('##')) for token in tokenized_sent] + [0]
+    input_ids = torch.LongTensor([input_ids])
+    input_ids = input_ids[:, :args.max_length].to(args.device)
+    token_starts = torch.ByteTensor([token_starts])
+    token_starts = token_starts[:, :args.max_length].to(args.device)
+    outputs = model(
+        input_ids=input_ids,
+        token_starts=token_starts,
+    )
+    logits = outputs[0]
+    entities = []
+    words = []
+    # A SMALL TRICK FOR TEST
+    # since our training data has much denser label
+    # while testing data has much sparser label
+    # we want to modify the logits to increase the rate of "O" label
+    logits[:, :, 0] += 7
+    # ==========================
+    if args.model_type == 'bert':
+        preds = torch.argmax(logits, dim=-1)[0].tolist()
+    else:
+        preds = model.crf.decode(logits)[0]
+
+    token_starts = [1 - int(token.startswith('##')) for token in tokenized_sent]
+    for token_start, token in zip(token_starts, tokenized_sent):
+        if token_start == 0:
+            entities.append('O')
+            words.append(token.replace('##', ''))
+        else:
+            entities.append(args.id2entity[preds.pop(0)])
+            words.append(token)
+
+    assert len(entities) == len(words)
+    return words, entities
+
+
+def inference(args, model, tokenizer):
+    def unk_wrapper(word):
+        return tokenizer.decode(tokenizer.encode(word), skip_special_tokens=True)
+
+    model.load_state_dict(
+        torch.load(
+            os.path.join(args.ckpt_save_dir, 'best_{}4{}.ckpt'.format(args.model_type, args.task))
+    ))
+    with open(args.output_file, 'w', newline='') as output_f, open(args.inference_file, 'r') as input_f:
+        sents = input_f.readlines()
+        for sent in tqdm(sents):
+            words = sent.strip().split(' ')
+            src_words, src_entities = ner_pipeline(args, sent, model, tokenizer)
+            tgt_words = []
+            tgt_entities = []
+            src_index = 0
+            tgt_index = 0
+            while src_index < len(src_words):
+                output_word = words[tgt_index]
+                output_entity = src_entities[src_index]
+                tgt_words.append(src_words[src_index])
+                tgt_entities.append(src_entities[src_index])
+                matcher = unk_wrapper(words[tgt_index])
+                matchee = unk_wrapper(tgt_words[-1])
+                src_index += 1
+                tgt_index += 1
+                while matcher != matchee:
+                    tgt_words[-1] += src_words[src_index]
+                    src_index += 1
+                    matchee = unk_wrapper(tgt_words[-1])
+                output_f.write(output_word + '\t' + output_entity + '\n')
+            output_f.write('\n')
+    return
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--timestamp', type=str, default=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(round(time.time()*1000))/1000)))
+    parser.add_argument('--model_type', type=str, default='t5', choices=['t5'])
+    parser.add_argument('--model_name', type=str, default='t5-small')
+    parser.add_argument('--have_cached_dataset', action='store_true')
+    parser.add_argument('--cache_dir', type=str, default='./cache')
+    parser.add_argument('--train_file', type=str, default='./data/WebQA_sub_data/train.jsonl')
+    parser.add_argument('--val_file', type=str, default='./data/WebQA_sub_data/val.jsonl')
+    parser.add_argument('--test_file', type=str, default='./data/val.jsonl')
+    parser.add_argument('--output_file', type=str, default='./data/submission.jsonl')
+    parser.add_argument('--task', type=str, default='webqa-finetune', choices=['webqa-finetune'])
+    parser.add_argument('--load_from_ckpt', type=str, default=None)
+    parser.add_argument('--model_chosen_metric', type=str, default='em')
+    parser.add_argument('--ckpt_save_dir', type=str, default='./checkpoints/')
+    parser.add_argument('--train_batch_size', type=int, default=4)
+    parser.add_argument('--gradient_accumulation_step', type=int, default=4)
+    parser.add_argument('--dev_batch_size', type=int, default=4)
+    parser.add_argument('--test_batch_size', type=int, default=4)
+    parser.add_argument('--encoder_max_length', type=int, default=512)
+    parser.add_argument('--decoder_max_length', type=int, default=128)
+    parser.add_argument('--num_epochs', type=int, default=10)
+    parser.add_argument('--learning_rate', type=float, default=5e-5)
+    parser.add_argument('--crf_learning_rate', type=float, default=5e-2)
+    parser.add_argument('--optimizer_type', type=str, default='adamw')
+    parser.add_argument('--scheduler_type', type=str, default='cosine')
+    parser.add_argument('--seed', type=int, default=42)
+    parser.add_argument('--device', type=str, default='cuda')
+    parser.add_argument('--dataset', type=str, default='webqa')
+    parser.add_argument('--train', action='store_true')
+    parser.add_argument('--inference', action='store_true')
+    parser.add_argument('--evaluation_steps', type=int, default=50)
+    parser.add_argument('--use_wandb', action='store_true')
+    parser.add_argument('--max_norm', type=float, default=5.0)
+    parser.add_argument('--weight_decay', type=float, default=0.01)
+    parser.add_argument('--use_fp16', action='store_true')
+    parser.add_argument('-n', '--id2entity', nargs='+', default=[
+        'O',
+        'B-MethodName', 'I-MethodName', 'B-HyperparameterName', 'I-HyperparameterName',
+        'B-HyperparameterValue', 'I-HyperparameterValue', 'B-MetricName', 'I-MetricName',
+        'B-MetricValue', 'I-MetricValue', 'B-TaskName', 'I-TaskName', 'B-DatasetName', 'I-DatasetName',
+    ])
+    args = parser.parse_args()
+    set_seed(args)
+    set_wandb(args)
+
+
+    tokenizer = attach_tokenizer(args)
+    model = attach_model(args)
+
+    if args.train:
+        train(args, model, tokenizer)
+    elif args.inference:
+        inference(args, model, tokenizer)
