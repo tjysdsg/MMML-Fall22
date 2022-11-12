d869c2cb64fc20665270a31cb594738a74b2fd86
tjysdsg <tjy1018543509@163.com>
2022-11-12 12:46:05 -0500

    Refactor retrieval prediction analysis code, include f1 calculation

--- utils/analyze_retrieval_predictions.py
+++ utils/analyze_retrieval_predictions.py
@@ -1,6 +1,7 @@
 import json
 from argparse import ArgumentParser
 import numpy as np
+import evaluate
 
 
 def get_args():
@@ -8,6 +9,105 @@ def get_args():
     parser.add_argument('--preds', type=str, default=r'vlp/result/baseline_predictions_val.json')
     parser.add_argument('--labels', type=str, default=r'E:\webqa\data\WebQA_train_val.json')
     return parser.parse_args()
+
+
+class RetrievalStatsCollector:
+    def __init__(self, n_questions: int):
+        self.n_questions = n_questions
+        self.num_sources = {}
+        self.full_correct_q = 0
+        self.overshoot_q = 0
+        self.undershoot_q = 0
+        self.correct_per_qcate = {}
+        self.n_incorrect = 0
+        self.n_multi_source = 0
+        self.correct_multi_source = 0
+        self.n_single_source = 0
+        self.correct_single_source = 0
+
+        self.gth_labels = []
+        self.pred_labels = []
+
+    def update_f1(self, positive_sources, negative_sources, pred_sources):
+        self.gth_labels += [1 for _ in range(len(positive_sources))] + [0 for _ in range(len(negative_sources))]
+        for source in positive_sources:
+            if source in pred_sources:
+                self.pred_labels.append(1)
+            else:
+                self.pred_labels.append(0)
+        for source in negative_sources:
+            if source in pred_sources:
+                self.pred_labels.append(1)
+            else:
+                self.pred_labels.append(0)
+        assert len(self.gth_labels) == len(self.pred_labels)
+
+    def update(self, pred_sources, question_meta):
+        pred_sources = [str(p) for p in pred_sources]
+        n_pred = len(pred_sources)
+
+        positive_sources = [str(f['image_id']) for f in question_meta['img_posFacts']]
+        positive_sources += [str(f['snippet_id']) for f in question_meta['txt_posFacts']]
+        negative_sources = [str(f['image_id']) for f in question_meta['img_negFacts']]
+        negative_sources += [str(f['snippet_id']) for f in question_meta['txt_negFacts']]
+
+        self.update_f1(positive_sources, negative_sources, pred_sources)
+
+        n_true = len(positive_sources)
+        self.num_sources.setdefault(n_pred, 0)
+        self.num_sources[n_pred] += 1
+
+        qcate = question_meta['Qcate']
+        self.correct_per_qcate.setdefault(qcate, [])
+
+        # statistics
+        if n_pred == n_true:  # same number of sources
+            if set(pred_sources) == set(positive_sources):  # fully correct
+                self.full_correct_q += 1
+                self.correct_per_qcate[qcate].append(1)
+
+                if n_true >= 2:
+                    self.correct_multi_source += 1
+                else:
+                    self.correct_single_source += 1
+            else:  # but incorrect
+                self.n_incorrect += 1
+                self.correct_per_qcate[qcate].append(0)
+        else:  # different number of sources
+            self.n_incorrect += 1
+            self.correct_per_qcate[qcate].append(0)
+            if n_pred > n_true:
+                self.overshoot_q += 1
+            elif n_pred < n_true:
+                self.undershoot_q += 1
+
+        if n_true >= 2:
+            self.n_multi_source += 1
+        else:
+            self.n_single_source += 1
+
+    def summary(self):
+        print('# of sources', {k: self.num_sources[k] for k in sorted(self.num_sources)})
+        print(f'Full correct questions: {self.full_correct_q / self.n_questions}')
+        print(f'Undershoot questions: {self.undershoot_q / self.n_questions}')
+        print(f'Overshoot questions: {self.overshoot_q / self.n_questions}')
+
+        print(f'Correct out of multi source: {self.correct_multi_source / self.n_multi_source}')
+        print(f'Correct out of single source: {self.correct_single_source / self.n_single_source}')
+
+        correct_per_qcate = {k: np.mean(v) for k, v in self.correct_per_qcate.items()}
+        print(f'correct per cate: {correct_per_qcate}')
+
+        # classification evaluation metrics
+        f1_metric = evaluate.load('f1')
+        recall_metric = evaluate.load('recall')
+        precision_metric = evaluate.load('precision')
+        results = f1_metric.compute(predictions=self.pred_labels, references=self.gth_labels)
+        print(f'F1: {results["f1"]}')
+        results = recall_metric.compute(predictions=self.pred_labels, references=self.gth_labels)
+        print(f'Recall: {results["recall"]}')
+        results = precision_metric.compute(predictions=self.pred_labels, references=self.gth_labels)
+        print(f'Precision: {results["precision"]}')
 
 
 def main():
@@ -18,67 +118,13 @@ def main():
     with open(args.labels, 'r', encoding='utf-8') as f:
         labels = json.load(f)
 
-    num_sources = {}
-    full_correct_q = 0
-    overshoot_q = 0
-    undershoot_q = 0
-    correct_per_qcate = {}
-    n_incorrect = 0
-    n_multi_source = 0
-    correct_multi_source = 0
-    n_single_source = 0
-    correct_single_source = 0
+    stats = RetrievalStatsCollector(len(preds))
+
     for q, data in preds.items():
-        sources = [str(s) for s in data['sources']]
-        n = len(sources)
-        num_sources.setdefault(n, 0)
-        num_sources[n] += 1
+        pred_sources = [str(s) for s in data['sources']]
+        stats.update(pred_sources, labels[q])
 
-        # if n > 3:
-        #     print(q)
-        meta = labels[q]
-
-        qcate = meta['Qcate']
-        correct_per_qcate.setdefault(qcate, [])
-        true_sources = [str(f['image_id']) for f in meta['img_posFacts']]
-        n_true = len(true_sources)
-
-        if n == n_true:  # same number of sources
-            if set(sources) == set(true_sources):  # fully correct
-                full_correct_q += 1
-                correct_per_qcate[qcate].append(1)
-
-                if n_true >= 2:
-                    correct_multi_source += 1
-                else:
-                    correct_single_source += 1
-            else:  # but incorrect
-                n_incorrect += 1
-                correct_per_qcate[qcate].append(0)
-        else:  # different number of sources
-            n_incorrect += 1
-            correct_per_qcate[qcate].append(0)
-            if n > n_true:
-                overshoot_q += 1
-            elif n < n_true:
-                undershoot_q += 1
-
-        if n_true >= 2:
-            n_multi_source += 1
-        else:
-            n_single_source += 1
-
-    n_questions = len(preds)
-    print('# of sources', {k: num_sources[k] for k in sorted(num_sources)})
-    print(f'Full correct questions: {full_correct_q / n_questions}')
-    print(f'Undershoot questions: {undershoot_q / n_questions}')
-    print(f'Overshoot questions: {overshoot_q / n_questions}')
-
-    print(f'Correct out of multi source: {correct_multi_source / n_multi_source}')
-    print(f'Correct out of single source: {correct_single_source / n_single_source}')
-
-    correct_per_qcate = {k: np.mean(v) for k, v in correct_per_qcate.items()}
-    print(f'correct per cate: {correct_per_qcate}')
+    stats.summary()
 
 
 if __name__ == '__main__':
