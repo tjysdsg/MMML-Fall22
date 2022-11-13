aaca560b0d2a0c96ec7f950ce71c337e9e790bd7
tjysdsg <tjy1018543509@163.com>
2022-11-13 16:21:05 -0500

    Create error subsets (common, vlp/roberta unique)

---
+++ error_analysis/get_common_unique_errors.py
@@ -0,0 +1,47 @@
+import json
+import os
+from argparse import ArgumentParser
+
+
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument('--vlp', type=str, required=True)
+    parser.add_argument('--roberta', type=str, required=True)
+    parser.add_argument('--out-dir', type=str, required=True)
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    out_dir = args.out_dir
+    os.makedirs(out_dir, exist_ok=True)
+
+    with open(args.vlp, 'r', encoding='utf-8') as f:
+        vlp = json.load(f)
+    with open(args.roberta, 'r', encoding='utf-8') as f:
+        roberta = json.load(f)
+
+    vlp_questions = set(vlp.keys())
+    roberta_questions = set(roberta.keys())
+
+    common = {}
+    vlp_unique = {}
+    roberta_unique = {}
+    for k in vlp_questions & roberta_questions:
+        common[k] = vlp[k]
+
+    for k in vlp_questions - roberta_questions:
+        vlp_unique[k] = vlp[k]
+    for k in roberta_questions - vlp_questions:
+        roberta_unique[k] = roberta[k]
+
+    with open(os.path.join(out_dir, 'common_error_subset.json'), 'w', encoding='utf-8') as f:
+        json.dump(common, f)
+    with open(os.path.join(out_dir, 'vlp_unique_error_subset.json'), 'w', encoding='utf-8') as f:
+        json.dump(vlp_unique, f)
+    with open(os.path.join(out_dir, 'roberta_unique_error_subset.json'), 'w', encoding='utf-8') as f:
+        json.dump(roberta_unique, f)
+
+
+if __name__ == '__main__':
+    main()
