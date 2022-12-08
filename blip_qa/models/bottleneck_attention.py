35187a4389917cb61540bee59c57e9b2cb65c002
tjysdsg <tjy1018543509@163.com>
2022-12-07 22:55:10 -0500

    scratch code for bottleneck attention

--- blip_qa/models/bottleneck_attention.py
+++ blip_qa/models/bottleneck_attention.py
@@ -1,13 +1,13 @@
 class AB:
     def __init__(self):
-        self.vb_att = cross_att()
-        self.tb_att = cross_att()
+        self.vb_att = BertAttention()
+        self.tb_att = BertAttention()
 
     def forward(self, vit_output, text_output, bottleneck=None):  # per layer
         if bottleneck is None:
             bottleneck = guassian(batch, bt_size)
 
-        vb = self.vb_att(vit_output, bottleneck)
-        tb = self.tb_att(text_output, bottleneck)
+        vb = self.vb_att(hidden_states=vit_output, encoder_hidden_states=bottleneck)[0]
+        tb = self.tb_att(hidden_states=text_output, encoder_hidden_states=bottleneck)[0]
 
         return ffn((vb + tb) / 2)
